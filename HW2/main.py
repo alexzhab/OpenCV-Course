f4553cd1100a7fc34199ad0c2b22af32a59ee8bc
Zhabitskaya Alexandra <yasashochek@gmail.com>
2024-10-23 21:26:00 +0300

    fix: data paths updated

--- HW2/main.py
+++ HW2/main.py
@@ -2,6 +2,6 @@ from chromakey import ChromaKey
 
 if __name__ == "__main__":
     chromakey = ChromaKey((640, 480),
-                          "data/door.mp4",
-                          "data/beach.jpg")
+                          "HW2/data/door.mp4",
+                          "HW2/data/beach.jpg")
     chromakey.process()
