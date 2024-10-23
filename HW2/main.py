32d58ff5be14a53954d22a18a95abab608143561
Alexandra Zhabitskaya <yasashochek@gmail.com>
2024-10-23 15:59:19 +0300

    upd: main.py with ChromaKey object

--- HW2/main.py
+++ HW2/main.py
@@ -1,31 +1,7 @@
-import cv2 as cv
-import numpy as np
+from chromakey import ChromaKey
 
 if __name__ == "__main__":
-    video_filename = "video.mp4"
-    video = cv.VideoCapture(video_filename)
-
-    while True:
-
-        ret, frame = video.read()
-
-        frame = cv.resize(frame, (640, 480))
-        image = cv.resize(image, (640, 480))
-
-        u_green = np.array([104, 153, 70])
-        l_green = np.array([30, 30, 0])
-
-        mask = cv.inRange(frame, l_green, u_green)
-        res = cv.bitwise_and(frame, frame, mask = mask)
-
-        f = frame - res
-        f = np.where(f == 0, image, f)
-
-        cv.imshow("video", frame)
-        cv.imshow("mask", f)
-
-        if cv.waitKey(25) == 27:
-            break 
-
-    video.release()
-    cv.destroyAllWindows()
+    chromakey = ChromaKey((640, 480),
+                          "/home/sasha/Downloads/door.mp4",
+                          "/home/sasha/Downloads/beach.jpg")
+    chromakey.process()
