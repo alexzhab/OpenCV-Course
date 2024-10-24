9465326599363663743b464a1cb39faa3c5fee5f
Zhabitskaya Alexandra <yasashochek@gmail.com>
2024-10-24 23:18:53 +0300

    upd: test video in main

--- HW2/main.py
+++ HW2/main.py
@@ -2,6 +2,6 @@ from chromakey import ChromaKey
 
 if __name__ == "__main__":
     chromakey = ChromaKey((640, 480),
-                          "HW2/data/door.mp4",
+                          "HW2/data/news.mp4",
                           "HW2/data/beach.jpg")
     chromakey.process()
